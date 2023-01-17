4ba2e20fbe79eaf4aeb9252b82a1ce1eec2fc65d
Pavaruth <pavaruth.buranakitsin@gmail.com>
2023-01-17 00:23:08 +0000

    4.1 Using Templates Done!

---
+++ tango_with_django_project/playground.py
@@ -0,0 +1,12 @@
+import os
+
+
+print(__file__)
+print(os.path.dirname(__file__))
+print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+print(BASE_DIR)
+print(TEMPLATE_DIR)
